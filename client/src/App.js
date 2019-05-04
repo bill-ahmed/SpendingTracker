96791a5ba5085a2e600fa22a83a1452cfedeb11d
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2019-05-04 13:50:40 -0400

    updated app to use SSL + requirements.txt for pip

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
     }
 
     const fetch = require('node-fetch');
-    fetch("http://127.0.0.1:5000/test", options)
+    fetch("https://127.0.0.1:5000/test", options)
     .then(res => {
       console.log(res);
       return res.json()})
--- server.py
+++ server.py
@@ -13,4 +13,4 @@ def test():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000, ssl_context="adhoc")
