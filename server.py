45da67a055fd12e7a2afe2957d81e159652acb29
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2019-05-04 15:10:57 -0400

    added connection for firestore database

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import sys
 import os
 
 app = Flask(__name__)
@@ -7,6 +11,21 @@ app = Flask(__name__)
 # Enable CORS
 CORS(app)
 
+'''*** Initialize connection to Firebase databse (firestore) ***'''
+# Use a service account
+cred = credentials.Certificate('./serviceAccount.json')
+firebase_admin.initialize_app(cred)
+
+# Initialize db
+db = firestore.client()
+
+users_ref = db.collection(u'users')
+docs = users_ref.stream()
+
+for doc in docs:
+    print(u'{} => {}'.format(doc.id, doc.to_dict()))
+
+'''*** API routes ***'''
 @app.route('/test', methods = ['GET'])
 def test():
     # Format the response as JSON object
